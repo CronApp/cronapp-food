c64483876782624b09b109038b60b75ae043767b
Paulo Roberto Balmer <paulo.balmer@cronapp.io>
2021-09-22 18:04:45 +0000

    Ajustes para chamar a tela do restaurante a partir do produto

--- src/main/java/blockly/Restaurante.java
+++ src/main/java/blockly/Restaurante.java
@@ -51,11 +51,6 @@ public static Var getLogoRestaurante(@ParamMetaData(description = "idProduto") V
 
    public Var call() throws Exception {
 
-    System.out.println(
-    Var.valueOf(
-    Var.valueOf("ID RECEBIDO").toString() +
-    idProduto.toString()).getObjectAsString());
-
     lista =
     cronapi.database.Operations.query(Var.valueOf("app.entity.ItemCardapio"),Var.valueOf("select i.restaurante from ItemCardapio i where i.id = :id"),Var.valueOf("id",idProduto));
 
@@ -73,11 +68,55 @@ cronapi.object.Operations.getObjectField(item, Var.valueOf("logotipo"));
  * @return Var
  */
 // Descreva esta função...
+public static Var getRestaurantePorIdProduto(@ParamMetaData(description = "id2") Var id2) throws Exception {
+ return new Callable<Var>() {
+
+   private Var item = Var.VAR_NULL;
+   private Var retorno = Var.VAR_NULL;
+
+   public Var call() throws Exception {
+
+    retorno =
+    cronapi.database.Operations.query(Var.valueOf("app.entity.ItemCardapio"),Var.valueOf("select i.restaurante from ItemCardapio i where i.id = :id"),Var.valueOf("id",id2));
+
+    item =
+    cronapi.json.Operations.createObjectJson();
+
+    cronapi.json.Operations.setJsonOrMapField(item,
+    Var.valueOf("nome"),
+    cronapi.object.Operations.getObjectField(retorno, Var.valueOf("nome")));
+
+    cronapi.json.Operations.setJsonOrMapField(item,
+    Var.valueOf("logotipoRestaurante"),
+    cronapi.object.Operations.getObjectField(retorno, Var.valueOf("logotipo")));
+
+    cronapi.json.Operations.setJsonOrMapField(item,
+    Var.valueOf("id"),
+    cronapi.object.Operations.getObjectField(retorno, Var.valueOf("id")));
+
+    cronapi.json.Operations.setJsonOrMapField(item,
+    Var.valueOf("taxaEntrega"),
+    cronapi.object.Operations.getObjectField(retorno, Var.valueOf("taxaEntrega")));
+
+    cronapi.json.Operations.setJsonOrMapField(item,
+    Var.valueOf("tempoEntrega"),
+    cronapi.object.Operations.getObjectField(retorno, Var.valueOf("tempoEntrega")));
+    return item;
+   }
+ }.call();
+}
+
+/**
+ *
+ * @param @ParamMetaData
+ * @return Var
+ */
+// Descreva esta função...
 public static Var getRestaurentePorId(@ParamMetaData(description = "id2") Var id2) throws Exception {
  return new Callable<Var>() {
 
-   private Var idProduto = Var.VAR_NULL;
    private Var lista = Var.VAR_NULL;
+   private Var idProduto = Var.VAR_NULL;
    private Var item = Var.VAR_NULL;
    private Var listRestaurantes = Var.VAR_NULL;
    private Var restauranteLogado = Var.VAR_NULL;
