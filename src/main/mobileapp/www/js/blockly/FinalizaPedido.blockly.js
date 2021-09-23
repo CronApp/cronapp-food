fdfeccc046a964f4025f5c698de04e151ec804db
Paulo Roberto Balmer <paulo.balmer@cronapp.io>
2021-09-23 13:36:36 +0000

    Ajustes na formatação das máscaras dos valores monetários, e inclusão do texto "Grátis" quando o frete não tem valor

--- src/main/mobileapp/www/js/blockly/FinalizaPedido.blockly.js
+++ src/main/mobileapp/www/js/blockly/FinalizaPedido.blockly.js
@@ -8,7 +8,7 @@ window.blockly.js.blockly.FinalizaPedido = window.blockly.js.blockly.FinalizaPed
  */
 window.blockly.js.blockly.FinalizaPedido.finalizarPedidoArgs = [];
 window.blockly.js.blockly.FinalizaPedido.finalizarPedido = async function() {
-
+ var retornoCep;
   await this.cronapi.util.callServerBlocklyNoReturn('blockly.Carrinho:fecharPedido', this.cronapi.screen.getValueOfField("FormaPagamento.active.id"), this.cronapi.screen.getValueOfField("vars.cep"), this.cronapi.screen.getValueOfField("vars.rua"), this.cronapi.screen.getValueOfField("vars.numero"), this.cronapi.screen.getValueOfField("vars.bairro"), this.cronapi.screen.getValueOfField("vars.cidade"), this.cronapi.screen.getValueOfField("vars.uf"));
   this.cronapi.screen.changeView("#/app/logged/home-pedidos",[ { teste : null } ]);
 }
@@ -18,7 +18,7 @@ window.blockly.js.blockly.FinalizaPedido.finalizarPedido = async function() {
  */
 window.blockly.js.blockly.FinalizaPedido.buscaCepArgs = [];
 window.blockly.js.blockly.FinalizaPedido.buscaCep = async function() {
-
+ var retornoCep;
   this.cronapi.util.getCEP(this.cronapi.screen.getValueOfField("vars.cep"), async function(sender_retornoCep) {
       retornoCep = sender_retornoCep;
     this.cronapi.screen.changeValueOfField("vars.rua", this.cronapi.object.getProperty(retornoCep, 'logradouro'));
--- src/main/mobileapp/www/js/blockly/Restaurante.blockly.js
+++ src/main/mobileapp/www/js/blockly/Restaurante.blockly.js
@@ -8,8 +8,18 @@ window.blockly.js.blockly.Restaurante = window.blockly.js.blockly.Restaurante ||
  */
 window.blockly.js.blockly.Restaurante.voltarArgs = [];
 window.blockly.js.blockly.Restaurante.voltar = async function() {
- var item;
+ var taxa, taxaCalculada;
   this.cronapi.screen.back();
+}
+
+/**
+ * Descreva esta função...
+ */
+window.blockly.js.blockly.Restaurante.converteStringEmFloatArgs = ['taxa'];
+window.blockly.js.blockly.Restaurante.converteStringEmFloat = async function(taxa) {
+ var taxaCalculada;
+  taxaCalculada = (taxa * 1);
+  return taxaCalculada;
 }
 
 /**
@@ -17,7 +27,7 @@ window.blockly.js.blockly.Restaurante.voltar = async function() {
  */
 window.blockly.js.blockly.Restaurante.inicializarTelaArgs = [];
 window.blockly.js.blockly.Restaurante.inicializarTela = async function() {
- var item;
+ var taxa, taxaCalculada;
   item = await this.cronapi.util.callServerBlockly('blockly.Restaurante:getRestaurentePorId', this.cronapi.screen.getValueOfField("params.idRestaurante"));
   this.cronapi.screen.changeValueOfField("vars.nomeRestaurante", this.cronapi.object.getObjectField(item, 'nome'));
 }
