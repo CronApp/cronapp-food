3a0504a8a8415875b40ab34c69fb54fd4c88ed8a
Paulo Roberto Balmer <paulo.balmer@cronapp.io>
2021-09-24 20:20:10 +0000

    Correção de bug que não mostrava o total da sacola de forma correta

--- src/main/mobileapp/www/js/blockly/Carrinho.blockly.js
+++ src/main/mobileapp/www/js/blockly/Carrinho.blockly.js
@@ -11,3 +11,13 @@ window.blockly.js.blockly.Carrinho.definirLoginUsuario = async function() {
 
   this.cronapi.screen.changeValueOfField("params.usuarioLogado", await this.cronapi.util.callServerBlockly('blockly.FuncoesUsuario:LoginDoUsuario'));
 }
+
+/**
+ * Descreva esta função...
+ */
+window.blockly.js.blockly.Carrinho.initTelaComTotaisArgs = [];
+window.blockly.js.blockly.Carrinho.initTelaComTotais = async function() {
+
+  this.cronapi.screen.changeValueOfField("params.totalFrete", await this.cronapi.util.callServerBlockly('blockly.Carrinho:totalFreteCarrinho'));
+  this.cronapi.screen.changeValueOfField("params.totalItens", await this.cronapi.util.callServerBlockly('blockly.Carrinho:totalCarrinho'));
+}
